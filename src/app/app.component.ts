73f5068d8781f5873452e29a449bd0a252a7c95a
Saurabh Gokhale <saurabh_gokhale@apple.com>
2021-03-27 17:17:30 -0700

    UI changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
+import { interval } from 'rxjs';
+import { ElasticsearchService } from './elasticsearch-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'iPredict';
+  uuid = '';
+  returnObj = [];
+  
+
+  constructor(private es: ElasticsearchService){
+    interval(2000).subscribe(x => {
+        this.uuid = uuidv4();
+        //console.log(this.uuid); 
+        this.es.getAllSolutions()
+        .then(response => {
+          console.log(response);
+          this.returnObj = response;
+        }, error => {
+          console.error(error);
+        }).then(() => {
+          console.log('Show Product Completed!');
+        });
+        /*
+        this.es.getAllProducts('opes', 'phone')
+          .then(response => {
+            //this.productSources = response.hits.hits;
+            this.returnObj = response.hits.hits;
+            console.log(this.returnObj[0]._source.search_data[0].full_text);
+          }, error => {
+            console.error(error);
+          }).then(() => {
+            console.log('Show Product Completed!');
+          });
+          */
+      });
+ }
+  ngOnInit(): void {
+    this.es.getAllProducts('opes', 'phone')
+      .then(response => {
+        //this.productSources = response.hits.hits;
+        this.returnObj = response.hits.hits;
+        //console.log(this.returnObj[0]._source.search_data[0].full_text);
+      }, error => {
+        console.error(error);
+      }).then(() => {
+        console.log('Show Product Completed!');
+      });
+  }
+
+  getBgColor(val): any {
+    return 'green';
+  }
+
+  getColor(val): any {
+    return 'white';
+  }
+  
 }
---
+++ src/app/elasticsearch-service.ts
@@ -0,0 +1,258 @@
+import { Injectable } from '@angular/core';
+import { Client } from 'elasticsearch-browser';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ElasticsearchService {
+
+  private client: Client;
+
+  financialData = [{
+    "radarId":"3452644",
+    "solutions":[{"radarId":"23423",
+                  "matching":"85"}, 
+                  {"radarId":"7345389",
+                  "matching":"40"}]
+  },
+  {
+    "radarId":" 9898793",
+    "solutions":[{"radarId":"454336",
+                  "matching":"62"}, 
+                  {"radarId":"7323289",
+                  "matching":"50"}]
+  }];
+
+  getAllSolutions(): any { 
+    //return this.financialData;
+    return Promise.resolve(this.financialData);
+  }
+
+  private queryalldocs = {
+    'query': {
+      'match_all': {}
+    }
+  };
+
+  /*private queryallproducts = {
+    '_source': ['search_result_data.name','search_result_data.preview_image','search_data.full_text'],
+    'query': {
+      'match_all': {}
+    }
+  };*/
+  private queryallproducts = {
+    '_source': [],
+    'query': {
+      'function_score': {
+        'query': { 'match_all': {} },
+        'boost': '5',
+        'functions': [
+          {
+            'script_score': {
+              'script': {
+                'source': "(1 + _score * 0.5)* doc['scores.stock'].value * (0.1 * doc['scores.random'].value + 0.3 * doc['scores.top_seller'].value + 0.1 * doc['scores.pdp_impressions'].value + 0.2 * doc['scores.sale_impressions_rate'].value + 0.1 * doc['scores.data_quality'].value + 0.3 * doc['scores.delivery_speed'].value)"
+              }
+            }
+          }
+        ]
+      }
+    }
+  };
+
+
+  constructor() {
+    if (!this.client) {
+      this.connect();
+    }
+  }
+
+  private connect() {
+    this.client = new Client({
+      //host: 'http://localhost:9200',
+      host: 'http://elastic:MyPassword@localhost:9200',
+      log: 'trace',
+      //auth: {
+      //  username: 'elastic',
+      //  password: 'MyPassword'
+      //}
+    });
+  }
+
+  isAvailable(): any {
+    return this.client.ping({
+      requestTimeout: Infinity,
+      body: 'Elasticsearch test'
+    });
+  }
+
+  addToIndex(value): any {
+    return this.client.create(value);
+  }
+
+  getAllDocuments(_index, _type): any {
+    return this.client.search({
+      index: _index,
+      type: _type,
+      body: this.queryalldocs,
+      filterPath: ['hits.hits._source']
+    });
+  }
+
+  getAllProducts(_index, _type): any {
+    return this.client.search({
+      index: _index,
+      type: _type,
+      body: this.queryallproducts,
+      filterPath: ['hits.hits._source']
+    });
+  }
+
+  getAllDocumentsWithScroll(_index, _type, _size): any {
+    return this.client.search({
+      index: _index,
+      type: _type,
+      scroll: '1m',
+      filterPath: ['hits.hits._source', 'hits.total', '_scroll_id'],
+      body: {
+        'size': _size,
+        'query': {
+          'match_all': {}
+        },
+        'sort': [
+          { '_uid': { 'order': 'asc' } }
+        ]
+      }
+    });
+  }
+
+  getNextPage(scroll_id): any {
+    return this.client.scroll({
+      scrollId: scroll_id,
+      scroll: '1m',
+      filterPath: ['hits.hits._source', 'hits.total', '_scroll_id']
+    });
+  }
+
+  fullTextSearch(_index, _type, _field, _queryText): any {
+    return this.client.search({
+      index: _index,
+      type: _type,
+      filterPath: ['hits.hits._source', 'hits.total', '_scroll_id'],
+      body: {
+        'query': {
+          'nested': {
+            'path': 'search_data',
+            'query': {
+              'match': {
+                [_field]: _queryText,
+              }
+            }
+          }
+        }
+      },
+      '_source': []
+    });
+  }
+
+  searchSuggest(_index, _type, _field, _queryText): any {
+    return this.client.search({
+      index: _index,
+      type: _type,
+      filterPath: [],
+      body: {
+        'size': 0,
+        'suggest': {
+          'searchsuggest': {
+            'text': _queryText,
+            'term': {
+              'field': _field
+            }
+          }
+        }
+      },
+      '_source': []
+    });
+  }
+
+  aggregationSearch(_index, _type): any {
+    return this.client.search({
+      index: _index,
+      type: _type,
+      body: {
+        'size': 0,
+        "aggs": {
+          "agg_string_facet": {
+            "nested": {
+              "path": "search_data.string_facets"
+            },
+            "aggs": {
+              "facet_make": {
+                "terms": { "field": "search_data.string_facets.facet-name" },
+
+                "aggs": {
+                  "facet_values": {
+                    "terms": { "field": "search_data.string_facets.facet-value" }
+                  }
+                }
+              }
+            }
+          }
+        }
+      },
+      '_source': []
+    });
+  }
+
+
+  aggregationSearchWithFilter(_index, _type, category, key): any {
+    return this.client.search({
+      index: _index,
+      type: _type,
+      body: {
+        "query": {
+          "bool": {
+            "must": [
+              {
+                "nested": {
+                  "path": "search_data.string_facets",
+                  "query": {
+                    "bool": {
+                      "must": [
+                        {
+                          "match": {
+                            "search_data.string_facets.facet-name": category
+                          }
+                        },
+                        {
+                          "match": {
+                            "search_data.string_facets.facet-value": key
+                          }
+                        }
+                      ]
+                    }
+                  }
+                }
+              }
+            ]
+          }
+        },
+        "aggregations": {
+          "agg_string_facet": {
+            "nested": {
+              "path": "search_data.string_facets"
+            },
+            "aggregations": {
+              "facet_make": {
+                "terms": {
+                  "field": "search_data.string_facets.facet-name"
+                }
+              }
+            }
+          }
+        }
+      },
+      '_source': []
+    });
+  }
+}
